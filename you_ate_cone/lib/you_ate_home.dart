9fb71e33e385ce29a17ed822f9ca263b933ca1bf
Peter Balint <potty0@gmail.com>
2020-05-23 16:26:23 +0200

    Default code cleanup, added initial home.

---
+++ you_ate_cone/lib/you_ate_home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class YouAteHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('You ate!'),);
+  }
+}
