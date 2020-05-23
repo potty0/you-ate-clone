9831d69b0536d90e38908eee5ce6b3fa0bfec790
Peter Balint <potty0@gmail.com>
2020-05-23 16:41:05 +0200

    Added Scaffold with bottom nav bar.

--- you_ate_cone/lib/you_ate_home.dart
+++ you_ate_cone/lib/you_ate_home.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class YouAteHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('You ate!'),);
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text('You ate!')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.face), title: Text('Profile')),
+        BottomNavigationBarItem(icon: Icon(Icons.stars), title: Text('Capture')),
+        BottomNavigationBarItem(icon: Icon(Icons.message), title: Text('Chat')),
+      ]),
+    );
   }
 }
