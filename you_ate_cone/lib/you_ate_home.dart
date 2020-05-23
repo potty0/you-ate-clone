4b6bab6bcb4c6c026a8a17ba44fe06279c11450e
Peter Balint <potty0@gmail.com>
2020-05-23 21:35:42 +0200

    Naming tweaks, refactor.

--- you_ate_cone/lib/you_ate_home.dart
+++ you_ate_cone/lib/you_ate_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youatecone/capture_overview_landing.dart';
 
 class YouAteHome extends StatefulWidget {
   @override
@@ -16,7 +17,7 @@ class _YouAteHomeState extends State<YouAteHome> {
         index: _selectedTabIndex,
         children: [
           Profile(),
-          Capture(),
+          CaptureOverviewLanding(),
           Chat(),
         ],
       ),
@@ -39,91 +40,6 @@ class Profile extends StatelessWidget {
     return Container(
       color: Colors.blue,
       child: Center(child: Icon(Icons.face, size: 200)),
-    );
-  }
-}
-
-class Capture extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView.separated(
-        itemBuilder: (context, index) => CaptureItem(
-          offTrack: index % 2 == 0,
-        ),
-        separatorBuilder: (context, index) => Divider(thickness: 0, height: 0),
-        itemCount: 100,
-      ),
-    );
-  }
-}
-
-class CaptureItem extends StatelessWidget {
-  final bool offTrack;
-
-  const CaptureItem({Key key, this.offTrack = false}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 160,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Flexible(flex: 2, child: Container()),
-          Flexible(flex: 5, child: buildCapture()),
-          Flexible(flex: 2, child: _buildTimeIndicator()),
-        ],
-      ),
-    );
-  }
-
-  Widget buildCapture() {
-    return Stack(fit: StackFit.expand, children: [
-      _buildStraightPath(),
-      _buildImage(),
-    ]);
-  }
-
-  Widget _buildStraightPath() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(width: 8, color: Colors.grey[300]),
-    ]);
-  }
-
-  Widget _buildImage() {
-    const double offTrackOffset = 40;
-    final double padding = offTrack ? offTrackOffset * 2 : 0;
-
-    return Container(
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.only(right: padding),
-          child: Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.grey,
-              boxShadow: [BoxShadow(blurRadius: 20, spreadRadius: 1, color: Colors.black45)],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTimeIndicator() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 12.0),
-          child: Text('HH:mm', textAlign: TextAlign.right),
-        )
-      ],
     );
   }
 }
