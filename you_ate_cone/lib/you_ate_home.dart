4c631dfb8492704792bce0bf41236596c7f0813b
Peter Balint <potty0@gmail.com>
2020-05-23 21:31:33 +0200

    Added stack container for capture item with path underneath, minor tweaks.

--- you_ate_cone/lib/you_ate_home.dart
+++ you_ate_cone/lib/you_ate_home.dart
@@ -51,7 +51,7 @@ class Capture extends StatelessWidget {
         itemBuilder: (context, index) => CaptureItem(
           offTrack: index % 2 == 0,
         ),
-        separatorBuilder: (context, index) => Divider(thickness: 0),
+        separatorBuilder: (context, index) => Divider(thickness: 0, height: 0),
         itemCount: 100,
       ),
     );
@@ -65,9 +65,6 @@ class CaptureItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double offTrackOffset = 40;
-    final double padding = offTrack ? offTrackOffset * 2 : 0;
-
     return Container(
       height: 160,
       child: Row(
@@ -75,40 +72,58 @@ class CaptureItem extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Flexible(flex: 2, child: Container()),
-          Flexible(
-            flex: 5,
-            child: Container(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(right: padding),
-                  child: Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey,
-                      boxShadow: [BoxShadow(blurRadius: 20, spreadRadius: 1, color: Colors.black45)],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: Text('HH:mm', textAlign: TextAlign.right),
-                )
-              ],
-            ),
-          ),
+          Flexible(flex: 5, child: buildCapture()),
+          Flexible(flex: 2, child: _buildTimeIndicator()),
         ],
       ),
+    );
+  }
+
+  Widget buildCapture() {
+    return Stack(fit: StackFit.expand, children: [
+      _buildStraightPath(),
+      _buildImage(),
+    ]);
+  }
+
+  Widget _buildStraightPath() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(width: 8, color: Colors.grey[300]),
+    ]);
+  }
+
+  Widget _buildImage() {
+    const double offTrackOffset = 40;
+    final double padding = offTrack ? offTrackOffset * 2 : 0;
+
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.only(right: padding),
+          child: Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey,
+              boxShadow: [BoxShadow(blurRadius: 20, spreadRadius: 1, color: Colors.black45)],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTimeIndicator() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 12.0),
+          child: Text('HH:mm', textAlign: TextAlign.right),
+        )
+      ],
     );
   }
 }
