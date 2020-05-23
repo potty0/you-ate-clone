9455620ad2271d1a3f6bfa0f266033cfc8a93c41
Peter Balint <potty0@gmail.com>
2020-05-23 21:10:29 +0200

    Added off track shift for content.

--- you_ate_cone/lib/you_ate_home.dart
+++ you_ate_cone/lib/you_ate_home.dart
@@ -48,7 +48,9 @@ class Capture extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListView.separated(
-        itemBuilder: (context, _) => CaptureItem(),
+        itemBuilder: (context, index) => CaptureItem(
+          offTrack: index % 2 == 0,
+        ),
         separatorBuilder: (context, index) => Divider(thickness: 0),
         itemCount: 100,
       ),
@@ -57,33 +59,43 @@ class Capture extends StatelessWidget {
 }
 
 class CaptureItem extends StatelessWidget {
+  final bool offTrack;
+
+  const CaptureItem({Key key, this.offTrack = false}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    const double offTrackOffset = 40;
+    final double padding = offTrack ? offTrackOffset * 2 : 0;
+
     return Container(
       height: 160,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Flexible(flex: 1, child: Container()),
+          Flexible(flex: 2, child: Container()),
           Flexible(
-            flex: 2,
+            flex: 5,
             child: Container(
               child: Center(
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.grey,
-                    boxShadow: [BoxShadow(blurRadius: 20, spreadRadius: 1, color: Colors.black45)],
+                child: Padding(
+                  padding: EdgeInsets.only(right: padding),
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey,
+                      boxShadow: [BoxShadow(blurRadius: 20, spreadRadius: 1, color: Colors.black45)],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
           Flexible(
-            flex: 1,
+            flex: 2,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
